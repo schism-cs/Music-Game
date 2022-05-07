e7faf5b087cb0b960287d6de503e76ac2b001c19
Christian Santoni <csantoni@keybiz.it>
2022-05-07 17:01:28 +0200

    Initial project commit

---
+++ Assets/NoteTargetController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteTargetController : MonoBehaviour
+{
+
+    public KeyCode activationKey;
+    public Color inactiveColor;
+    public Color activeColor;
+    
+    private bool _isActive;
+    private SpriteRenderer _spriteRenderer;
+
+    private void Start()
+    {
+        _isActive = false;
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.color = inactiveColor;
+    }
+
+    private void Update()
+    {
+        _spriteRenderer.color = _isActive ? activeColor : inactiveColor;
+
+        _isActive = Input.GetKey(activationKey);
+        
+        
+    }
+}
